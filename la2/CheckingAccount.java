b4e26cf7bb878c188989039eb932794010fb03ea
Tony Nielsen <tonynielsen@u.boisestate.edu>
2022-10-17 22:13:57 -0600

    Solid Progress

--- la2/CheckingAccount.java
+++ la2/CheckingAccount.java
@@ -1,11 +1,12 @@
 public class CheckingAccount extends Account {
 
     public CheckingAccount(String number, Customer customer, double balance) {
-	this.number=number;
-	this.customer=customer;
-	this.balance=balance;
+        this.number = number;
+        this.customer = customer;
+        this.balance = balance;
     }
 
-    public void accrue(double rate) {}
+    public void accrue(double rate) {
+    }
 
 }
